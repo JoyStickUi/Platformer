554af9294ae2fe5c7a61cb99e240d3f5c96ad443
Карих Сергей Владимирович <karix.2017@stud.nstu.ru>
2020-10-02 23:54:31 +0700

    Main gameplay mechanics integration
    Movement, returning to spawn when fall out scene, camera pursuing, level end trigger.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject ball;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - ball.transform.position;
+    }
+
+    void Pursue(){
+        transform.position = ball.transform.position + offset;
+    }
+
+    void Update()
+    {
+        Pursue();
+    }
+}
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    [Header("Скорость вращения")]
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }    
+
+    void Movement(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+}
